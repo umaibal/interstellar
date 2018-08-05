d9312cfabc73a98d11a8a7bf5abd70419953c3d7
umaibal <umai.balendra@shopify.com>
2018-08-05 14:06:25 -0400

    add unit tests

--- test/models/document_test.rb
+++ test/models/document_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class DocumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "document attributes must not be empty" do
+    document = Document.new
+    assert document.invalid?
+    assert document.errors[:document_type].any?
+    assert document.errors[:expiration_date].any?
+  end
 end
--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class FlightTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "flight attributes must not be empty" do
+    flight = Flight.new
+    assert flight.invalid?
+    assert flight.errors[:seat].any?
+    assert flight.errors[:destination].any?
+    assert flight.errors[:departure_time].any?
+    assert flight.errors[:arrival_time].any?
+  end
+
+  test "flight seat must be valid" do 
+    flight = Flight.new(destination: "Ottawa",
+                        departure_time: '13:00:00',
+                        arrival_time: '14:00:00')
+
+    flight.seat = "hik"         
+    assert flight.invalid?
+    
+    flight.seat = 3
+    assert flight.valid?
+  end
 end
